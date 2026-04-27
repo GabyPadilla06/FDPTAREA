74c594b04e39c367ba8e8e46a1f8ecfd39b9cc0a
GabyPadilla06 <00028626@uca.edu.sv>
2026-04-27 16:41:58 -0600

    :sparkles: Ejercicio 3 terminado

---
+++ Semana1/Ejercicio3/Ejercicio3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num1, num2;
+
+    cout << "Bienvenido a la calculadora de numeros" << endl;
+    cout << "Ingresa el primer numero" << endl;
+    cin >> num1;
+
+    cout << "Ingresa el segundo numero" << endl;
+    cin >> num2;
+
+    if(num1 > num2){
+        cout << num1 << " es mayor que " << num2 << endl;
+    }else if(num1 < num2){
+        cout << num1 << " es menor que " << num2 << endl;
+    }else{
+        cout << num1 << " es igual que " << num2 << endl;
+    }
+
+    return 0;
+}
