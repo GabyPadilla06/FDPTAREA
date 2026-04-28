38dba268424e5b6d931d787841f3f9c6ca6fb31b
GabyPadilla06 <00028626@uca.edu.sv>
2026-04-28 14:52:45 -0600

    :sparkles: Ejercicio 16 terminado

---
+++ Semana1/Ejercicio16/Ejercicio16.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    string usuario;
+    string contrasena;
+    int numeroIntentos = 3;
+
+    string usuarioPredeterminado = "Diana";
+    string contrasenaPredeterminada = "Diana#2006";
+
+    cout << "Bienvenido, inicia sesion" << endl;;
+    cout << "Por favor ingresa tu nombre de usuario: " << endl;
+    cin >> usuario;
+
+    cout << "Por favor ingresa tu password: " << endl;
+    cin >> contrasena;
+
+    if(usuario == usuarioPredeterminado && contrasena == contrasenaPredeterminada){
+        cout << "Inicio de sesion exitoso, bienvenid@ " << usuario << endl; 
+    }else{
+        cout << "Credenciales incorrectas. Te quedan 2 intentos" << endl;
+
+        cout << "Por favor ingresa tu nombre de usuario: " << endl;
+        cin >> usuario;
+        cout << "Por favor ingresa tu password: " << endl;
+        cin >> contrasena;
+
+        if(usuario == usuarioPredeterminado && contrasena == contrasenaPredeterminada){
+            cout << "Inicio de sesion exitoso, bienvenid@ " << usuario << endl;
+        }else{
+            cout << "Credenciales incorrectas. Te queda 1 intento" << endl;
+
+            cout << "Por favor ingresa tu nombre de usuario: " << endl;
+            cin >> usuario;
+            cout << "Por favor ingresa tu password: " << endl;
+            cin >> contrasena;
+
+            if(usuario == usuarioPredeterminado && contrasena == contrasenaPredeterminada){
+                cout << "Inicio de sesion exitoso, bienvenid@ " << usuario << endl;
+            }else{
+                cout << "Lo sentimos, pero fallaste todos tus intentos, prueba mas tarde" << endl;
+            }
+        }
+    }
+
+    return 0;
+}
