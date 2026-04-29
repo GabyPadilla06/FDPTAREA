ff4ec4351f58774522f5d91f64e6728127e627c6
GabyPadilla06 <00028626@uca.edu.sv>
2026-04-29 11:52:34 -0600

    Modificaciones del ejercicio 10

--- Semana1/Ejercicio10/Ejercicio10.cpp
+++ Semana1/Ejercicio10/Ejercicio10.cpp
@@ -2,21 +2,35 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
 
     float pesoPaquete;
+    int cobro, billetera = 500;
 
     cout << "Bienvenido al programa de paqueteria" << endl;
     cout << "Ingresa el peso de tu paquete en kg: " << endl;
     cin >> pesoPaquete;
 
-    if(pesoPaquete <= 5.0){
-        cout << "El costo total de tu paquete es de $5" << endl;
-    }else if(pesoPaquete >= 6.0 && pesoPaquete <= 10.0){
-        cout << "El costo total de tu paquete es de $10" << endl;
-    }else{
-        cout << "El costo total de tu paquete es de $15" << endl;
+    if (pesoPaquete > 0 && pesoPaquete <= 5)
+    {
+        cobro = billetera - 5;
+        cout << "Su cobro se realizo correctamente, su nuevo saldo es de: " << cobro << endl;
     }
-    
+    else if (pesoPaquete >= 6.0 && pesoPaquete <= 10.0)
+    {
+        cobro = billetera - 10;
+        cout << "Su cobro se realizo correctamente, su nuevo saldo es de: " << cobro << endl;
+    }
+    else if (pesoPaquete > 10.0)
+    {
+        cobro = billetera - 15;
+        cout << "Su cobro se realizo correctamente, su nuevo saldo es de: " << cobro << endl;
+    }
+    else
+    {
+        cout << "Ingresa un numero valido" << endl;
+    }
+
     return 0;
 }
